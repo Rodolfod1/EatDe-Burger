559cfb4535161b81225dfd52ec44aff0f6a3887d
Rodolfo Diaz <rodolfodzr@gmail.com>
2020-12-13 18:15:28 -0500

    database and seeds ready

---
+++ config/connection.js
@@ -0,0 +1,27 @@
+//Creating connections and exporting them 
+
+var mysql = require("mysql");
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// MySQL DB Connection Information (remember to change this with our specific credentials)
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "N12rocks@01",
+  database: "burger"
+});
+
+// Initiate MySQL Connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+// exporting connection 
+module.exports=connection;
