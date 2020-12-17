d470192025d084b6dd4258537f90063ac8a3fc8e
Rodolfo Diaz <rodolfodzr@gmail.com>
2020-12-17 13:01:23 -0500

    adding jaws

--- config/connection.js
+++ config/connection.js
@@ -7,14 +7,20 @@ var mysql = require("mysql");
 var PORT = process.env.PORT || 8080;
 
 // MySQL DB Connection Information (remember to change this with our specific credentials)
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "N12rocks@01",
-  database: "burger_db"
-});
+var connection;
 
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "N12rocks@01",
+    database: "burger_db"
+  });
+};
+  
 // Initiate MySQL Connection.
 connection.connect(function(err) {
   if (err) {
